919e0a0ed8c3ed9ac3d80fcc0ff29f54b0ac9cb7
jonathom <jonathanbahlmann@web.de>
2019-07-22 20:16:02 +0200

    added JSON call

--- public/javascripts/meeting.js
+++ public/javascripts/meeting.js
@@ -1,3 +1,16 @@
 // jshint esversion: 6
 
 //do two routes meet?
+
+//get JSON from DB
+$.getJSON('/users/routes', function(data) {
+  routesJSON = data;
+  var tableContent ='';
+  $.each(data, function(index) {
+
+//for each, do
+
+  });
+
+  $('#resultTable').html(tableContent);
+});
