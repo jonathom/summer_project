747e5de4350ba6ea9d21244c893a354ae917b0e5
jonathom <jonathanbahlmann@web.de>
2019-07-23 21:32:58 +0200

    meeting point calc now evals all routes

--- public/javascripts/meeting.js
+++ public/javascripts/meeting.js
@@ -2,82 +2,135 @@
 
 //do two routes meet?
 
-//get JSON from DB
-$.getJSON('/users/routes', function(data) {
-  routesJSON = data;
-  var tableContent ='';
-  $.each(data, function(index) {
-
-//for each, do
-
+/**
+  * function getAllRoutes is button response function for calculate meetings button
+  * gets JSON routes from db
+  * calls next function
+  * @author Jonathan Bahlmann
+  */
+function getAllRoutes() {
+  let routesJSON = [];
+  $.getJSON('/users/routes', function(data) {
+    //for each, do
+    $.each(data, function(index) {
+      routesJSON.push(JSON.stringify(this));
+    });
+    checkForMeetings(routesJSON);
   });
 
-  $('#resultTable').html(tableContent);
-});
+}
 
+var meetingPointsArray = [];
+
+function checkForMeetings(allRoutes) {
+  //allRoutes is array with route-strings in JSON format
+  //get firstLine from input textarea
+  var firstLine = document.getElementById("routeInQuestion").value;
+
+  //calculate meeting Points for all routes
+  for(let i = 0; i < allRoutes.length; i++) {
+    if(firstLine != allRoutes[i]) {
+      lineStringsIntersect(firstLine, allRoutes[i]);
+    }
+  }
+}
+
+var apiKey = '21381ccbb60531b0ec9d57038076849a';
+
+//array of featureCollections
+var meetingsArray = [];
 /**
   * @desc Checks if two choosen routes are intersecting eachother
   * Uses turf.js
   * @see https://turfjs.org/
   * @author Benjamin Rieke 408743
   */
-function lineStringsIntersect() {
-var firstLine = document.getElementById("outputRoute1").value;
-var secondLine = document.getElementById("outputRoute2").value;
-var l1 = JSON.parse(firstLine).features[0].geometry;
-var l2 = JSON.parse(secondLine).features[0].geometry;
-var firstName = JSON.parse(firstLine).username;
-var firstType = JSON.parse(firstLine).routeType;
-var secondName = JSON.parse(secondLine).username;
-var secondType = JSON.parse(secondLine).routeType;
+function lineStringsIntersect(firstLine, secondLine) {
+  console.log("lSI called with 1: " + JSON.parse(firstLine).name + " 2: " + JSON.parse(secondLine).name);
 
+  //lines geometry
+  var l1 = JSON.parse(firstLine);
+  var l2 = JSON.parse(secondLine);
 
+  //GeoJSON of intersections
+  var intersects = turf.lineIntersect(l1.features[0].geometry, l2.features[0].geometry);
 
-console.log(firstName);
-console.log(firstType);
+  // for each intersection do
+  for(let i = 0; intersects.features[i]; i++) {
+    intersects.features[i].properties.firstUsername = l1.username;
+    intersects.features[i].properties.secondUsername = l2.username;
+    intersects.features[i].properties.firstID = l1._id;
+    intersects.features[i].properties.secondID = l2._id;
+    intersects.features[i].properties.firstType = l1.type;
+    intersects.features[i].properties.secondType = l2.type;
+    console.log(JSON.stringify(intersects.features[i]));
+    var meetingPoint = (intersects.features[i].geometry.coordinates);
+    // switch lat and long for display on leaflet
+    intersects.features[i].geometry.coordinates = [meetingPoint[1], meetingPoint[0]];
+    //push to meetingsArray
+    meetingsArray.push(intersects.features[i]);
 
+    let thisPoint = intersects.features[i];
 
-var intersects = turf.lineIntersect(l1, l2);
-//console.log(intersects);
+    xhrGetWeather(thisPoint.geometry.coordinates[0], thisPoint.geometry.coordinates[1], handleWeather, thisPoint);
 
-// for each intersection
-for(let i = 0; intersects.features[i]; i++){
-var meetingPoint = (intersects.features[i].geometry.coordinates);
-// switch lat and long for display on leaflet
-[meetingPoint[0], meetingPoint[1]] = [meetingPoint[1], meetingPoint[0]];
-var lat = meetingPoint[0];
-var longit = meetingPoint[1];
-console.log(meetingPoint);
-
-var apiKey = '21381ccbb60531b0ec9d57038076849a';
-var weatherOutput = getWeather(lat, longit);
-// console.log(weatherOutput1);
-
-
-//JSON.stringify(weatherOutput);
-//And place them as a marker on the map
-placeMarker(meetingPoint, firstName, firstType, secondName, secondType, weatherOutput);
-console.log(weatherOutput);
-
-}
+    //JSON.stringify(weatherOutput);
+    //And place them as a marker on the map
+    //placeMarker(thisPoint.geometry.coordinates, thisPoint.firstuserName, thisPoint.firstType, thisPoint.secondserName, thisPoint.secondType, xhrGetWeather);
+    //console.log(weatherOutput);
+  }
 }
 
-function getWeather(lat, longit){
-  var weatherOutput1;
+/**
+  * this function handles the xhr request to openweathermap
+  * @author Joanthan Bahlmann
+  * @param lat Latitude
+  * @param lon Longitude
+  * @param cFunc callback function for successful request
+  * @param i index passed down into cFunc
+  */
+function xhrGetWeather(lat, lon, cFunc, meetingPoint) {
+  console.log("xhrGetWeather called");
+  var url = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+apiKey;
+  var xhttp = new XMLHttpRequest();
+  //when ready
+  xhttp.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+      cFunc(this, meetingPoint);
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+  console.log("xhr send");
+}
 
-  $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + longit + "&appid=" + "21381ccbb60531b0ec9d57038076849a", function(data) {
-    var temperature = data.main.temp;
-    var toCelsius = 273.1;
-    temperature = temperature - toCelsius;
+/**
+  * this function handles the xhttp response from OWMTOKEN
+  * it saves the weather into a table, creates a marker at the found weather position
+  * also the additional information xhr requests are called from here, depending on the weather code
+  * @author Jonathan Bahlmann
+  * @param xhttp xhttp responseText
+  * @param i index to save the responseText at the right place in table
+  */
+function handleWeather(xhttp, meetingPoint) {
+  //parse resonse
+  weatherXML = JSON.parse(xhttp.responseText);
+  let temperature = weatherXML.main.temp;
+  let toCelsius = 273.1;
+  temperature = temperature - toCelsius;
+  temperature = Math.round(temperature*10)/10;
+  let wString = '';
+  wString += weatherXML.weather[0].description + ',' + temperature + '°C';
 
-    temperature = Math.round(temperature*10)/10;
-     weatherOutput1  = data.weather[0].description+", "+temperature+"°C";
-     document.getElementById("JSONresponse").value = weatherOutput1;
+  var n1 = JSON.stringify(meetingPoint.properties.firstUsername);
+  var n2 = JSON.stringify(meetingPoint.properties.secondUsername);
+  var t1 = JSON.stringify(meetingPoint.properties.firstType);
+  var t2 = JSON.stringify(meetingPoint.properties.secondType);
+//TODO type ist featureCollection
+  console.log(t2);
 
-   //console.log(weatherOutput);
-});
-return weatherOutput1;
-console.log(weatherOutput1);
+  placeMarker(meetingPoint.geometry.coordinates, n1, n2, t1, t2, wString);
+
 }
 
 
@@ -86,76 +139,64 @@ console.log(weatherOutput1);
 * @param coordi The coordinates
 * @author Benjamin Rieke 408743
 */
-function placeMarker(coordi, vFirstName, vFirstType, vSecondName, vSecondtype, vWeather){
+function placeMarker(coordi, vFirstName, vFirstType, vSecondName, vSecondType, vWeather){
 var markerList = '';
 //If you have two planned routes that intersect
-if(vFirstName == vSecondName && vFirstType == vSecondtype && vFirstType == 'planned'){
+if(vFirstName == vSecondName && vFirstType == vSecondType && vFirstType == 'planned'){
      markerList = "You already planned parts of this route. Congrats!";
 }
 //If you have two routes. One that is planned and the other is already completed
-if(vFirstName == vSecondName && vFirstType == 'planned' && vFirstType != vSecondtype){
+if(vFirstName == vSecondName && vFirstType == 'planned' && vFirstType != vSecondType){
 markerList = "You will encounter yourself. Congrats!";
 console.log(markerList);
 }
 
 //If you have two routes. One that is already completed and the other is planned
-if(vFirstName == vSecondName && vFirstType == 'completed' && vFirstType != vSecondtype){
+if(vFirstName == vSecondName && vFirstType == 'completed' && vFirstType != vSecondType){
 markerList = "You´ve already been here. Congrats!";
 console.log(markerList);
 }
 
 // If your planned route intersects with somebody elses
-if(vFirstName != vSecondName && vFirstType == 'planned' && vFirstType == vSecondtype){
+if(vFirstName != vSecondName && vFirstType == 'planned' && vFirstType == vSecondType){
 markerList = "You might encounter " +  vSecondName+ " Say Hello!";
 console.log(markerList);
 }
 
 // If your planned route intersects with somebody elses
-if(vFirstName != vSecondName && vFirstType == 'planned' && vFirstType !=vSecondtype){
+if(vFirstName != vSecondName && vFirstType == 'planned' && vFirstType !=vSecondType){
 markerList = vSecondName+" was already here!";
 console.log(markerList);
 }
 
 // If your completed route intersected with somebody elses
-if(vFirstName != vSecondName && vFirstType == 'completed' && vFirstType ==vSecondtype){
+if(vFirstName != vSecondName && vFirstType == 'completed' && vFirstType ==vSecondType){
 markerList = "Your route intersected with " +  vSecondName+" Write him a message!";
 console.log(markerList);
 }
 
 // If your route intersected with somebody elses who has not completed it
 
-if(vFirstName != vSecondName && vFirstType == 'completed' && vFirstType !=vSecondtype){
+if(vFirstName != vSecondName && vFirstType == 'completed' && vFirstType !=vSecondType){
 markerList = "Your completed route might be intersected by " +  vSecondName+". Tell him if you liked it!";
 console.log(markerList);
 }
 
 //If an animal was on parts of your planned route
 
-if(vFirstType == 'planned' && vSecondtype == 'animal'){
+if(vFirstType == 'planned' && vSecondType == 'animal'){
 markerList = "You will follow the paths of " +  vSecondName+"! An Animal!" ;
 console.log(markerList);
 }
 
 // If you were on the same path as an animal
 
-if(vFirstType == 'completed' && vSecondtype == 'animal'){
+if(vFirstType == 'completed' && vSecondType == 'animal'){
 markerList = "You walked on the same paths as " +  vSecondName+"! An Animal! How wonderful!" ;
 console.log(markerList);
-  }
-  console.log(vWeather);
-/*
-  var temperature = vWeather.main.temp;
-  var toCelsius = 273.1;
-  var finalWeather;
-    temperature = temperature - toCelsius;
+}
 
-    temperature = Math.round(temperature*10)/10;
-     finalWeather  = vWeather.weather[0].description+", "+temperature+"°C";
-
-*/
 L.marker(coordi).addTo(map)
- .bindPopup(markerList + vWeather +'<button type="button" onclick="createRouteButton()" class="btn btn-dark">Share</button>');
- console.log(coordi);
-
+ .bindPopup(markerList + vWeather +'<br><button type="button" onclick="createRouteButton()" class="btn btn-dark">Share</button>');
 
 }
